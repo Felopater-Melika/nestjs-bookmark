98a1ce540e19b9e037c31245aa0424f66bfaaf82
Felopater Melika <felopatermelika@gmail.com>
2023-06-15 19:19:49 -0500

    finished bookmark api

---
+++ src/user/user.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { EditUserDto } from './dto';
+
+@Injectable()
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+
+  async editUser(userId: number, dto: EditUserDto) {
+    const user = await this.prisma.user.update({
+      where: { id: userId },
+      data: { ...dto },
+    });
+
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    delete user.hash;
+    return user;
+  }
+}
